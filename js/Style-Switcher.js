72c5cb92161f8c9f4e5db48559815f5df8b9e85d
clinton <uchecliton2020@gmail.com>
2022-08-18 02:42:21 +0100

    done with my portfolio development front-end

---
+++ js/Style-Switcher.js
@@ -0,0 +1,45 @@
+/** ============== toggle style switcher =============== **/
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("Open");
+})
+
+// hide style - switcher on scroll
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("Open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("Open");
+    }
+})
+/** ============== theme colors =============== **/
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute ("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled","true");
+        }
+    })
+}
+/** ============== Theme Light and Dark Mode =============== **/
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.toggle("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.toggle("fa-moon");
+    }
+})
